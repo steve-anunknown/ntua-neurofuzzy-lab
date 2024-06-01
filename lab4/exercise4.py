c9f52adf2ad2470586b469d00f3704f2ae4baf39
steve-anunknown <steve.anunknown@gmail.com>
2024-06-01 12:19:43 +0300

    i have to verify correctness here

--- lab4/exercise4.py
+++ lab4/exercise4.py
@@ -1,12 +1,15 @@
 import numpy as np
+from numba import jit
+
 
 # the policy is the choice that the controller makes for each state
+@jit(nopython=True)
 def compute_optimal_policy(g, num_states, discount_factor):
     # Initialize the value function array
     V = np.zeros(num_states)
     # Initialize the policy array
     policy = np.zeros(num_states, dtype=int)
-    
+
     # Tolerance for convergence
     tolerance = 1e-5
     delta = 1
@@ -31,20 +34,21 @@ def compute_optimal_policy(g, num_states, discount_factor):
             V[i] = min_value
 
     return V, policy
-   
+
 if __name__ == '__main__':
-	# The given g function
-	g = np.array([1, 2, 3, 4, 5, 4, 2, 0, 1, 2])
+    # The given g function
+    g = np.array([1, 2, 3, 4, 5, 4, 2, 0, 1, 2])
 
-	num_states = 10
+    num_states = 10
 
-	# different values of a
-	discount_factors = [0.2, 0.5, 0.7, 0.9]
-	optimal_policies = {}
+    # different values of a
+    base = 0.2
+    discount_factors = [base * (i + 1) for i in range(10)]
+    optimal_policies = {}
 
-	for a in discount_factors:
-	    V, policy = compute_optimal_policy(g, num_states, a)
-	    optimal_policies[a] = policy
+    for a in discount_factors:
+        V, policy = compute_optimal_policy(g, num_states, a)
+        optimal_policies[a] = policy
 
-	print(optimal_policies)
+    print(optimal_policies)
 
